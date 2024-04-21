e278c023c1c2329131a4ecbef93765294dddec58
sjostmeier <stefan.jostmeier@proton.me>
2024-04-21 19:02:04 +0200

    update code with function: get_chars_dict() and dict_to_sorted_list() + the print statements for the report

--- main.py
+++ main.py
@@ -2,7 +2,20 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     word_number = get_word_number(text)
-    print(f"The book contains {word_number} number of words")
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = dict_to_sorted_list(chars_dict)
+    
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_number} words found in the document")
+    print()
+
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was found {item['num']} times")
+
+    print("--- End report ---")
+
 
 
 def get_book_text(path):
@@ -13,6 +26,25 @@ def get_word_number(text):
     words = text.split()
     return len(words)
 
+def get_chars_dict(text):
+    chars = {}
+    for c in text:
+        lowered = c.lower()
+        if lowered in chars:
+            chars[lowered] += 1
+        else:
+            chars[lowered] = 1
+    return chars
+
+def sort_on(dict):
+    return dict["num"]
+
+def dict_to_sorted_list(chars_dict):
+    sorted_list = []
+    for char in chars_dict:
+        sorted_list.append({"char": char, "num": chars_dict[char]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
 
 
 main()
